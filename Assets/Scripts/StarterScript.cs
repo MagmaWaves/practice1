881530bfc1c33e016a40a40eb02b325996b694ba
MagmaWaves <luluduwors@gmail.com>
2025-04-17 11:56:04 -0700

    tutorial part 7 9:29

---
+++ Assets/Scripts/StarterScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class StarterScript : MonoBehaviour
+{
+    [SerializeField] int myNumber;
+    [SerializedField] string myName;
+    [SerializedField] bool myChoice;
+    [SerializedField] GameObject myGate;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
