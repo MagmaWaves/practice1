8c731c291063f82f9f64d49d23fe6b63580c2737
MagmaWaves <luluduwors@gmail.com>
2025-04-18 12:26:43 -0700

    1st person player tutorial 8 8:58

--- Assets/Scripts/StarterScript.cs
+++ Assets/Scripts/StarterScript.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class StarterScript : MonoBehaviour
 {
-    [SerializeField] int myNumber;
+    /*
+    [SerializedField] int myNumber;
     [SerializedField] string myName;
     [SerializedField] bool myChoice;
     [SeralizedField] GameObject myGate;
     [SeralizedField] int randomNumber;
+    */
     //why [] and why is it not highlighted green
 
     void Start()
